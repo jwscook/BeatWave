0e7abd89552a5d00761ab19fc130caeb1b58bdd9
James Cook <cookjws@gmail.com>
2023-01-04 12:08:33 +0000

    pave way for polarisation of ions

--- prepare_directories_and_decks.sh
+++ prepare_directories_and_decks.sh
@@ -11,3 +11,6 @@ for i in null wave1 wave2 both
 do
   cat $i.prefix.deck suffix.deck > $datestub/$i/input.deck
 done
+
+# Run via e.g.
+# echo 2023-01-04-09-39-23/both/ | mpirun -np 2 ../epoch/epoch1d/bin/epoch1d
