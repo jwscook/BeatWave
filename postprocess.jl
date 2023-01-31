b61a919b0d935778b26d3e1d61210f578dc185dd
James Cook <James Cook>
2023-01-31 10:35:18 +0000

    ready for first runs

--- postprocess.jl
+++ postprocess.jl
@@ -1,40 +1,73 @@
-using Plots, FFTW, PyCall, Statistics
+using Plots, FFTW, PyCall, Statistics, Plots
 
+function subtractinitialvalue!(x)
+  x .-= x[1]
+end
 
 NT = 2^10
 for suffix in ("null", "wave1", "wave2", "both")
-  dir = "./scratch/" * suffix * "/"
+  @show dir = "./scratch/" * suffix * "/"
   
   sdf = pyimport("sdf")
-  F = zeros(8192, NT);
+  h = sdf.read(dir * lpad(0, 5, "0") * ".sdf")
+  Ex0 = h.Electric_Field_Ex_averaged.data
+  NG = length(Ex0)
+  F = zeros(NG, NT);
   fEx(h) = h.Electric_Field_Ex_averaged.data
   fEy(h) = h.Electric_Field_Ey_averaged.data
   fEz(h) = h.Electric_Field_Ez_averaged.data
   fBy(h) = h.Magnetic_Field_By_averaged.data
   fBz(h) = h.Magnetic_Field_Bz_averaged.data
 
+  energydensityelectrons = zeros(NT)
+  energydensityions = zeros(NT)
+  energydensityfields = zeros(NT)
+  L = h.Grid_Grid.data[1][end]
+  dx = L / NG
+
+  for i in 0:size(F, 2)-1
+    h = sdf.read(dir * lpad(i, 5, "0") * ".sdf")
+    try
+      energydensityelectrons[i+1] = sum(h.Derived_Average_Particle_Energy_Electron.data) / L
+      energydensityions[i+1] = sum(h.Derived_Average_Particle_Energy_Ion.data) / L
+      energydensityfields[i+1] = h.Total_Field_Energy_in_Simulation__J_.data / L
+    catch e
+      @warn "Failed to read particle or field energy from $dir at step $i"
+    end
+  end
+  subtractinitialvalue!(energydensityelectrons)
+  subtractinitialvalue!(energydensityions)
+  subtractinitialvalue!(energydensityfields)
+  energydensitytotal = energydensityelectrons + energydensityions + energydensityfields
+  plot(energydensityelectrons, label="e")
+  plot!(energydensityions, label="i")
+  plot!(energydensityfields, label="f")
+  plot!(energydensitytotal, label="t")
+  savefig(dir * "energydensities.png")
+
+
   for (ffield, str) in ((fEx,"Ex"), (fEy, "Ey"), (fEz, "Ez"), (fBy, "By"), (fBz, "Bz"))
     for i in 0:size(F, 2)-1
       h = sdf.read(dir * lpad(i, 5, "0") * ".sdf")
-      F[:, i+1] .= ffield(h)
+      try
+        F[:, i+1] .= ffield(h)
+      catch e
+        @warn "File number $i has no $str"
+      end
     end
     
     filt = sin.(((1:size(F,2)) .- 0.5) ./ size(F,2) .* pi)';
-    heatmap(F[1:1024, 1:1024])
-    savefig(dir * str *"_TX_1024_1024.png")
-    heatmap(F[1:512, :])
-    savefig(dir * str *"_TX_512_$NT.png")
 
-    heatmap(log10.(abs.(reverse(fft(F)', dims=1))))
-    savefig(dir * str * "_WK_whole_unfiltered.png")
+    heatmap(F)
+    savefig(dir * str *"_TX.png")
 
-    #Z = reverse(fftshift(fft((F .- mean(F)) .* filt)[:, 1:end÷2], 1), dims=1)
-    Z = reverse(fftshift(fft((F .- mean(F)))[:, 1:end÷2], 1), dims=1)
+    Ffilt = F .* filt
+    nplt = 128 # number modes to plot int time / freq
+    nplx = 128 # number modes to plot int space / k
     # Z is now positive omega and k, in correct order. Would plot as k vs omega though
     # transpose to plot omega against k, log and abs for contrast
-    heatmap(log10.(abs.(Z)')[1:end÷2, (end÷4):(3*end÷4)])
-    #Z = reverse(fftshift(fft((F .- mean(F)) .* filt)), dims=1)
-    #heatmap(log10.(abs.(Z)'))
+    Z = log10.(abs.(reverse(fftshift(fft(Ffilt .- mean(Ffilt)))', dims=1)))
+    heatmap(-nplx:nplx, 0:nplt, Z[(end÷2):(end÷2 + nplt), (end÷2-nplx+1):(end÷2+nplx+1)])
     savefig(dir * str * "_WK.png")
   end
 end
